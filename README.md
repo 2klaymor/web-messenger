bf01958c27501f6c300813fdf817cbb729cd5778
kklaymor <2klaymor@gmail.com>
2024-10-24 21:39:54 +0300

    Первоначальный коммит

---
+++ README.md
@@ -0,0 +1,3 @@
+## web-messenger
+
+Веб приложение на Express + React для дипломной работы
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const PORT = 3001;
+
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+app.get('/api', (req, res) => {
+    res.json({ msg: 'test' });
+});

c14ed2b35236c6076cc0f86b754ed2e3eb9d2b7c
inktel <itachiplachet@gmail.com>
2025-03-18 23:53:49 +0300

    незначительные улучшения
    - контексты перемещены из App.js в index.js
    - ВЫЯВЛЕНА ПРИЧИНА ТРЯСКИ (МИГАНИЯ) ПРИ СМЕНЕ ТЕМЫ. ОТКЛЮЧЕН TRANSITION ДЛЯ КНОПОК BOOTSTRAP

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 
+import {ThemeProvider} from './utils/theme'
+import {LanguageProvider} from './utils/language';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -10,8 +12,13 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <LanguageProvider>
+            <ThemeProvider>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </ThemeProvider>
+        </LanguageProvider>
     </React.StrictMode>
+
 );

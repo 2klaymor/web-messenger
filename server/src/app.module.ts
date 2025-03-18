b95b47b5f063114e60bc21a9176df679a3e3bb90
kklaymor <2klaymor@gmail.com>
2025-03-18 21:55:11 +0300

    Мердж feature/user-module-final

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { ChatModule } from './chat/chat.module';
-import { MessageModule } from './message/message.module';
 import { RouterModule } from '@nestjs/core';
 
+import { UsersModule } from './users/users.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { ChatsModule } from './chats/chats.module';
+import { MessagesModule } from './messages/messages.module';
+import { AuthModule } from './auth/auth.module';
+
+
 @Module({
-  imports: [UserModule, ChatModule, MessageModule,
+  imports: [UsersModule, ChatsModule, MessagesModule, ContactsModule, AuthModule,
     RouterModule.register([
       {
         path: 'api/v1',
-        children: [UserModule, ChatModule, MessageModule]
+        children: [UsersModule, ChatsModule, ContactsModule, MessagesModule]
       }
     ])
   ],
